c892a3da71959800a4fff81d9251bb6eebb82bde
Andrés <a.paolosanabria@gmail.com>
2023-10-30 21:41:34 -0500

    comit de Andrés Sanabria

--- Main.java
+++ Main.java
@@ -32,8 +32,11 @@ public class Main {
 
 
 //3. Liste todos los desarrolladores que se unieron en el año 2023 (el año se extraerá del código del desarrollador, es decir, los primeros 4 caracteres)
+<<<<<<< HEAD
+=======
         System.out.println("<<<<<<<<<<<<Esta es la 3ra pregunta>>>>>>>>>>>>");
 
+>>>>>>> f09e9bec1988a8f2ff56ed0eda1f5ddd1458cb61
         int añoActual = 2023;
         System.out.println("Desarrolladores que se unieron en el año 2023:");
         for (Desarrollador desarrollador : employeeList) {
@@ -56,12 +59,75 @@ public class Main {
 
 
 //5. Imprima los nombres de todos los desarrolladores con el tercer salario más alto. (Generalícelo para el enésimo salario más alto).
+        //5. Imprima los nombres de todos los desarrolladores con el tercer salario más alto. (Generalícelo para el enésimo salario más alto).
+        List<Desarrollador> copiaDesarrolladores = new ArrayList<>(employeeList);
+
+
+        Collections.sort(copiaDesarrolladores, Comparator.comparingInt(Desarrollador::getSalario).reversed());
+
+
+        int nSalarioMasAlto = 3;
+
+
+        if (nSalarioMasAlto <= copiaDesarrolladores.size()) {
+
+            int salarioDeseado = copiaDesarrolladores.get(nSalarioMasAlto - 1).getSalario();
+
+
+            System.out.println("Desarrolladores con el " + nSalarioMasAlto + "° salario más alto (" + salarioDeseado + "):");
+            for (Desarrollador desarrollador : employeeList) {
+                if (desarrollador.getSalario() == salarioDeseado) {
+                    System.out.println(desarrollador.getNombres());
+                }
+            }
+        } else {
+            System.out.println("No hay suficientes desarrolladores para obtener el " + nSalarioMasAlto + "° salario más alto.");
+        }
 //6. Imprimir salario mínimo.
+        int salarioMinimo = Integer.MAX_VALUE;
+
+        for (Desarrollador desarrollador : employeeList) {
+            if (desarrollador.getSalario() < salarioMinimo) {
+                salarioMinimo = desarrollador.getSalario();
+            }
+        }
 //7. Imprima la lista de todos los desarrolladores con salario mínimo.
+        salarioMinimo = Integer.MAX_VALUE;
+
+        for (Desarrollador desarrollador : employeeList) {
+            if (desarrollador.getSalario() < salarioMinimo) {
+                salarioMinimo = desarrollador.getSalario();
+            }
+        }
+        System.out.println("Desarrolladores con salario mínimo (" + salarioMinimo + "):");
+        for (Desarrollador desarrollador : employeeList) {
+            if (desarrollador.getSalario() == salarioMinimo) {
+                System.out.println(desarrollador);
+            }
+        }
 
 //8. Liste a todas las personas que trabajan en más de 2 proyectos.
+        Map<Desarrollador, Integer> proyectosPorDesarrollador = new HashMap<>();
+
+
+        for (Desarrollador desarrollador : employeeList) {
+            proyectosPorDesarrollador.put(desarrollador, desarrollador.getIniciativas().size());
+        }
+
+
+        System.out.println("Personas que trabajan en más de 2 proyectos:");
+        for (Map.Entry<Desarrollador, Integer> entry : proyectosPorDesarrollador.entrySet()) {
+            if (entry.getValue() > 2) {
+                System.out.println(entry.getKey().getNombres() + " trabaja en " + entry.getValue() + " proyectos.");
+            }
+        }
 
 //9. Conteo del total de laptops asignadas a los desarrolladores.
+        int totalLaptopsAsignadas = 0;
+        for (Desarrollador desarrollador : employeeList) {
+            totalLaptopsAsignadas += desarrollador.getTotalLaptopsAsignados();
+        }
+        System.out.println("Total de laptops asignadas a los desarrolladores: " + totalLaptopsAsignadas);
 
 //10. Recuento de todas las iniciativas con Luis Carrillo Lopez.
         long contadorIniciativasLuisCarrilloLopez = employeeList.stream()
