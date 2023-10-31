7c7a588f086f810316689041c5f11efb29762abb
Jhersvin <jhersvinvillodasquinto@gmail.com>
2023-10-30 22:27:19 -0500

    Jhersvin Villodas Quinto

--- Main.java
+++ Main.java
@@ -27,16 +27,13 @@ public class Main {
         System.out.println("<<<<<<<<<<<<Esta es la 2nda pregunta>>>>>>>>>>>>");
 
 //2. Imprima el nombre completo de cualquier desarrollador cuyo nombre comience con "A".
-        List<String> Desarrollador= Arrays.asList("Bethy Alvarez", "Ana Castillo" ,"Pablo Castro", " Julisa Vivanco", "Anabel Requena", "Marco Serrano","Erick Taquiri", "Ronald Baltazar", "Diego Sanchez"," Carmen Calle");
-        Desarrollador.stream().filter(nombre->nombre.startsWith("A")).forEach(System.out::println);
+        List<String> Desarrolladoress= Arrays.asList("Bethy Alvarez", "Ana Castillo" ,"Pablo Castro", " Julisa Vivanco", "Anabel Requena", "Marco Serrano","Erick Taquiri", "Ronald Baltazar", "Diego Sanchez"," Carmen Calle");
+        Desarrolladoress.stream().filter(nombre->nombre.startsWith("A")).forEach(System.out::println);
 
 
+        System.out.println("------------------------------3------------------------------------------");
 //3. Liste todos los desarrolladores que se unieron en el año 2023 (el año se extraerá del código del desarrollador, es decir, los primeros 4 caracteres)
-<<<<<<< HEAD
-=======
-        System.out.println("<<<<<<<<<<<<Esta es la 3ra pregunta>>>>>>>>>>>>");
 
->>>>>>> f09e9bec1988a8f2ff56ed0eda1f5ddd1458cb61
         int añoActual = 2023;
         System.out.println("Desarrolladores que se unieron en el año 2023:");
         for (Desarrollador desarrollador : employeeList) {
@@ -46,9 +43,10 @@ public class Main {
                 System.out.println(desarrollador.getNombres() + " se unió en el año " + añoActual);
             }
         }
+
+        System.out.println("--------------------------------4----------------------------------------");
 //4. Ordene los desarrolladores según el nombre; y luego ordene por salario.
 
-        employeeList = employeeFactory.getAllDesarrolladores();
         System.out.println("<<<<<<<<<<<<Esta es la 4ta pregunta>>>>>>>>>>>>");
 
         for (Desarrollador desarrollador : employeeList) {
@@ -57,22 +55,17 @@ public class Main {
             System.out.println("----");
         }
 
-
+        System.out.println("------------------------------5------------------------------------------");
 //5. Imprima los nombres de todos los desarrolladores con el tercer salario más alto. (Generalícelo para el enésimo salario más alto).
-        //5. Imprima los nombres de todos los desarrolladores con el tercer salario más alto. (Generalícelo para el enésimo salario más alto).
-        List<Desarrollador> copiaDesarrolladores = new ArrayList<>(employeeList);
 
+        List<Desarrollador> copiaDesarrolladores = new ArrayList<>(employeeList);
 
         Collections.sort(copiaDesarrolladores, Comparator.comparingInt(Desarrollador::getSalario).reversed());
 
-
         int nSalarioMasAlto = 3;
-
-
         if (nSalarioMasAlto <= copiaDesarrolladores.size()) {
 
             int salarioDeseado = copiaDesarrolladores.get(nSalarioMasAlto - 1).getSalario();
-
 
             System.out.println("Desarrolladores con el " + nSalarioMasAlto + "° salario más alto (" + salarioDeseado + "):");
             for (Desarrollador desarrollador : employeeList) {
@@ -83,6 +76,8 @@ public class Main {
         } else {
             System.out.println("No hay suficientes desarrolladores para obtener el " + nSalarioMasAlto + "° salario más alto.");
         }
+
+        System.out.println("----------------------------------6--------------------------------------");
 //6. Imprimir salario mínimo.
         int salarioMinimo = Integer.MAX_VALUE;
 
@@ -91,6 +86,8 @@ public class Main {
                 salarioMinimo = desarrollador.getSalario();
             }
         }
+        System.out.println("El salario minimo es:"+salarioMinimo);
+        System.out.println("------------------------------------7------------------------------------");
 //7. Imprima la lista de todos los desarrolladores con salario mínimo.
         salarioMinimo = Integer.MAX_VALUE;
 
@@ -105,7 +102,7 @@ public class Main {
                 System.out.println(desarrollador);
             }
         }
-
+        System.out.println("--------------------------------8----------------------------------------");
 //8. Liste a todas las personas que trabajan en más de 2 proyectos.
         Map<Desarrollador, Integer> proyectosPorDesarrollador = new HashMap<>();
 
@@ -121,7 +118,7 @@ public class Main {
                 System.out.println(entry.getKey().getNombres() + " trabaja en " + entry.getValue() + " proyectos.");
             }
         }
-
+        System.out.println("-------------------------------9-----------------------------------------");
 //9. Conteo del total de laptops asignadas a los desarrolladores.
         int totalLaptopsAsignadas = 0;
         for (Desarrollador desarrollador : employeeList) {
@@ -129,6 +126,7 @@ public class Main {
         }
         System.out.println("Total de laptops asignadas a los desarrolladores: " + totalLaptopsAsignadas);
 
+        System.out.println("--------------------------------10----------------------------------------");
 //10. Recuento de todas las iniciativas con Luis Carrillo Lopez.
         long contadorIniciativasLuisCarrilloLopez = employeeList.stream()
                 .flatMap(desarrollador -> desarrollador.getIniciativas().stream())
@@ -136,6 +134,7 @@ public class Main {
                 .count();
         System.out.println("Número de iniciativas con Luis Carrillo Lopez: " + contadorIniciativasLuisCarrilloLopez);
 
+        System.out.println("------------------------------11------------------------------------------");
 //11. Lista de todas las personas que trabajan con Luis Carrillo Lopez.
         String nombreLuisito = "Luis Carrillo Lopez";
         List<Desarrollador> personasQueTrabajanConLuisCarrillo= employeeList.stream()
@@ -145,6 +144,7 @@ public class Main {
         System.out.println("Personas que trabajan con Luis Carrillo Lopez:");
         personasQueTrabajanConLuisCarrillo.forEach(desarrollador -> System.out.println(desarrollador.getNombres()));
 
+        System.out.println("-------------------------------12-----------------------------------------");
 //12. Cree un mapa basado en estos datos, el key debe ser el año de incorporación y el valor debe ser la lista de todos los desarrolladores que se incorporaron en ese año en particular.
 
         Map<Integer, List<Desarrollador>> desarrolladoresPorAño = desarrolladores.stream()
@@ -155,12 +155,14 @@ public class Main {
             listaDesarrolladores.forEach(desarrollador -> System.out.println("  - " + desarrollador.getNombres() + " " + desarrollador.getApellidos()));
         });
 
-//14. Cree un mapa basado en estos datos, el key debe ser el año deincorporación y el valor debe ser el recuento de personas que se unieron en ese añoen particular.
+        System.out.println("--------------------------------14----------------------------------------");
+
+//14. Cree un mapa basado en estos datos, el key debe ser el año de incorporación y el valor debe ser el recuento de personas que se unieron en ese añoen particular.
         Map<Integer, Long> conteoPorAño = desarrolladores.stream()
                 .collect(Collectors.groupingBy(d -> Integer.parseInt(d.getCodigo().substring(0, 4)), Collectors.counting()));
 
-        conteoPorAño.forEach((anio, cantidad) -> {
-            System.out.println("Año: " + anio + ", Cantidad: " + cantidad);
+        conteoPorAño.forEach((año, cantidad) -> {
+            System.out.println("Año: " + año + ", Cantidad: " + cantidad);
         });
     }
 }
